64fe20154ec85add33a4e61a5d02f6a75b5ddaec
Serhii SHEVCHUK <seshevch@e2r8p3.unit.ua>
2019-05-07 19:38:32 +0300

    04

---
+++ Unity_04/Assets/Scripts/EnterText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnterText : MonoBehaviour
+{
+    public InputField inputF;
+    public string inputText;
+    public GameObject Panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Panel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputText = inputF.textComponent.text;
+    }
+    public void SaveInput()
+    {
+        Panel.SetActive(false);
+    }
+}
---
+++ Unity_04/Assets/Scripts/Loading.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour
+{
+    public Text Text1;
+    public Text Text2;
+
+    void Start()
+    {
+        Load();
+    }
+
+    void Load()
+    {
+        //string key = GameManager.G_count_text.ToString();
+        string key1 = "1";
+        string key2 = "2";
+        if (PlayerPrefs.HasKey(key1 + "a") && PlayerPrefs.HasKey(key2 + "a"))
+        {
+            Text1.text = PlayerPrefs.GetString(key1 + "a");
+            Text2.text = PlayerPrefs.GetString(key2 + "a");
+        }
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+        //Debug.Log(key);
+    }
+}
