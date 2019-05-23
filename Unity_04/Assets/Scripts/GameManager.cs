bae2098a3a75d37eb5ed095cdcdd137abdec173a
Serhii SHEVCHUK <seshevch@e1r8p14.unit.ua>
2019-05-23 15:11:58 +0300

    zero

--- Unity_04/Assets/Scripts/GameManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class GameManager
-{
-    public static int G_count_text;
-    public static int G_main_choice;
-}
