cd66a16704180242d442d9404797c20e5b0c40c2
Serhii SHEVCHUK <seshevch@e1r7p3.unit.ua>
2019-06-02 16:15:43 +0300

    new

---
+++ Unity_08/Assets/Scripts/ClickMouse.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickMouse : MonoBehaviour
+{
+    
+    Rigidbody rb;
+
+    bool click = false;
+    bool stayCentre = false;
+
+
+    Vector3 posTarget;
+
+    //public Transform target;
+
+	private void Awake()
+	{
+        rb = GetComponent<Rigidbody>();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        //rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (click)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Vector3.zero, 25f * Time.deltaTime);
+            if (Mathf.Abs(transform.position.x) < 1.5f && Mathf.Abs(transform.position.z) < 1.5f)
+            {
+                Debug.Log("Stay");
+                rb.velocity = Vector3.zero;
+				stayCentre = true;
+				click = false;
+
+            }
+        }
+
+        if (stayCentre)
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, 15f);
+        }
+
+        if (Input.GetMouseButtonDown(0) && stayCentre)
+        {
+            Debug.Log("Click");
+            stayCentre = false;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                rb.mass = 1;
+                rb.AddForce(hit.point * 300f);
+            }
+        }
+
+        if (transform.position.x == 0f && transform.position.z == 0f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+    }
+
+    private void OnMouseUp()
+    {
+        click = true;
+	}
+}
---
+++ Unity_08/Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Vector3.zero, 1f * Time.deltaTime);
+        //if (transform.position.x == 0f && transform.position.z == 0f)
+        //{
+        //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //}
+    }
+}
